4e6103f5f8e72c53ce74d363b9953c25275632b7
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-05-07 22:48:51 +0530

    set counter app

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,14 +1,29 @@
-import H1 from "./H1";
+// import H1 from "./H1.jsx";
+// import Theme from "./Theme.jsx";
+// import GithubApi from "./GithubApi.jsx";
+
+import { useState } from "react";
 
 function App() {
+
+// 1. use Top level
+
+    const [count , setCount] = useState(0)
+
     return (
         <div>
-            <h1>App</h1>
-            <H1 title="{My name is : Joy}" desc="Intern"/>
+            <h1> {count} </h1>
+            <button onClick={() => setCount(count + 1) }>➕</button>
+            {/* <H1 title="{My name is : Joy}" desc="Intern"/>
             <H1 title="Heading-2" desc="Hello hello Ji!"/>
             <H1 title="Heading-3" desc="Description for Heading-3"/>
             <H1 title="Heading-4" desc="Description for Heading-4"/>
             <H1 title="Heading-5" desc="Description for Heading-5"/>
+            
+            
+            <Theme/>
+            <GithubApi/> */}
+
         </div>
     )
 }
