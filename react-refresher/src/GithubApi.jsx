5ab40cf360c1f05c336914ef784f91e2aa9219a2
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-05-22 20:50:54 +0530

    Added Food Delivery App project

---
+++ react-refresher/src/GithubApi.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+import { useState, useEffect } from 'react'
+
+const GithubApi = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+            }
+                catch (error) {
+                }
+                finally {
+
+                }
+                
+    }
+  , [])
+
+  return (
+    <div>GithubApi</div>
+  )
+}
+
+export default GithubApi
