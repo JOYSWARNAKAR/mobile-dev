9fc14b5cd18a69fc7d0757b20c6f611df18bb846
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-05-23 18:30:46 +0530

    feat: enhance DetailScreen to display username and update navigation options

--- React-navigation/src/navigator/stack/DynamicStackNavigator.tsx
+++ React-navigation/src/navigator/stack/DynamicStackNavigator.tsx
@@ -10,7 +10,21 @@ const Stack = createNativeStackNavigator()
 
 function MyStack() {
     return(
-        <Stack.Navigator>
+        <Stack.Navigator
+        //  Apply globalStyles
+        screenOptions={{
+            headerShown :true,
+                
+                headerStyle : {
+                    backgroundColor : "rgba(43, 41, 43, 0.66)",
+                },
+                headerTitleAlign: "center",
+                headerTintColor : "orange",
+                headerTitleStyle:{
+                    fontWeight: "bold",
+                    fontSize: 22, 
+                }
+        }}>
             <Stack.Screen name = 'Home' component = {HomeScreen} 
             options={{
                 headerShown :true,
--- React-navigation/src/screens/DetailScreen.tsx
+++ React-navigation/src/screens/DetailScreen.tsx
@@ -1,17 +1,25 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 
 import { useNavigation } from '@react-navigation/native';
 
-const DetailScreen = () => {
+const DetailScreen = ({route}: any) => {
 
-const navigation = useNavigation<any>()
+const navigation = useNavigation<any>();
+const username = route?.params?.username || "Guest";
+
+useLayoutEffect(() => {
+  navigation.setOptions({
+    title: "Details"
+  })
+}, [navigation]
+)
 
   return (
     <View>
-      <Text>DetailScreen</Text>
+      <Text> {username} </Text>
        {/* <Button title = 'Go back' onPress={() => navigation.goBack()} ></Button> */}
-       <Button title = 'Go to Profile' onPress={() => navigation.navigate("Profile")} ></Button>
+       <Button title = 'Go to Profile' onPress={() => navigation.replace("Profile")} ></Button>
     </View>
   )
 }
