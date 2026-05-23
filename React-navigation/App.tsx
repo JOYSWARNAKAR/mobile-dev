6046e4badb9a0439ad2e9fed9d22e8b8d436c901
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-05-23 19:11:53 +0530

    feat: add bottom tabs dependency and complete bottom tabs in static

--- React-navigation/App.tsx
+++ React-navigation/App.tsx
@@ -1,68 +1,63 @@
+// import * as React from 'react';
+// import { View, Text } from 'react-native';
+// import { createStaticNavigation } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import HomeScreen from './src/screens/HomeScreen';
+// import DetailScreen from './src/screens/DetailScreen';
+// import ProfileScreen from './src/screens/ProfileScreen';
+// import DynamicStackNavigator from './src/navigator/stack/DynamicStackNavigator';
+// import StaticStackNavigator from './src/navigator/stack/StaticStackNavigator';
+
+
+// export default function App(){
+
+//   // return <DynamicStackNavigator />
+//   // return <StaticStackNavigator />
+// }
+
+
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailScreen from './src/screens/DetailScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import DynamicStackNavigator from './src/navigator/stack/DynamicStackNavigator';
-import StaticStackNavigator from './src/navigator/stack/StaticStackNavigator';
+import { Text, View } from 'react-native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// const stack = createNativeStackNavigator({
-  
-//   // initialRouteName: 'Home',
-  
-//   screens : {
-//     Home: HomeScreen,
-//     Details: DetailScreen,
-//     Profile: ProfileScreen
-//   }
-// });
+function HomeScreen() {
+  const navigation = useNavigation();
 
-// const Navigation = createStaticNavigation(stack)
-
-export default function App(){
-  // return <Navigation/>
-
-  // return <DynamicStackNavigator />
-  return <StaticStackNavigator />
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')}>
+        Go to Profile
+      </Button>
+    </View>
+  );
 }
 
+function ProfileScreen() {
+  const navigation = useNavigation();
 
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
+    </View>
+  );
+}
 
+const MyTabs = createBottomTabNavigator({
+  screens: {
+    Home: HomeScreen,
+    Profile: ProfileScreen,
+  },
+});
 
+const Navigation = createStaticNavigation(MyTabs);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Home Screen</Text>
-//     </View>
-//   );
-// }
-
-// const RootStack = createNativeStackNavigator({
-//   screens: {
-//     Home: HomeScreen,
-//   },
-// });
-
-// const Navigation = createStaticNavigation(RootStack);
-
-// export default function App() {
-//   return <Navigation />;
-// }
-
+export default function App() {
+  return <Navigation />;
+}
